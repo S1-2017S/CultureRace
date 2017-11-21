f062b1923f7fefe54cd417ad337564704772118e
NIMZIL <NIMZIL@intechS1.fr>
2017-11-21 09:27:06 +0100

    modif

---
+++ req_commencer.js
@@ -0,0 +1,33 @@
+//commencer.js
+
+//=========================================================================
+// Traitement de "req_commencer"
+// Auteur : 
+// Version : 
+//=========================================================================
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query) {
+
+	var marqueurs;
+	var page;
+
+	// AFFICHAGE DE LA PAGE D'ACCUEIL
+
+	page = fs.readFileSync('acceuil.html', 'utf-8');
+
+	marqueurs = {};
+	marqueurs.erreur = "";
+	marqueurs.compte = "";
+	page = page.supplant(marqueurs);
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+};
+//--------------------------------------------------------------------------
+
+module.exports = trait;

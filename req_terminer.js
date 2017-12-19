6c71593b63da14cebd8c26c0c7ef7b7ebea274f5
NDJERE_YOMBO <NDJERE_YOMBO@intechS1.fr>
2017-12-19 09:25:05 +0100

    termin

--- req_terminer.js
+++ req_terminer.js
@@ -12,9 +12,34 @@ require('remedial');
 
 var trait = function (req, res, query) {
 
+   var marqueurs;
+   var contenu_fichier;
+   var listeConnectes = [];
+   var player;  
+
+   contenu_fichier = fs.readFileSync("connectes.json", "UTF-8");
+   listeConnectes = JSON.parse(contenu_fichier);
+
+
    //ON REVIENT A ACCUEIL MEMBRE
 
    page = fs.readFileSync('accueil_membre.html', 'UTF-8');
+   
+   player = {};
+   player.pseudo = query.pseudo;
+   player.etat = "LIBRE";
+
+   for(i=0; i<listeConnectes.length; i++) {
+      if(listeConnectes[i].pseudo === query.pseudo) {
+	      listeConnectes[i] = player;
+      }
+   }
+
+   contenu_fichier = JSON.stringify(listeConnectes);
+   fs.writeFileSync("connectes.json", contenu_fichier, "UTF-8");
+
+   marqueurs = {};
+   marqueurs.pseudo = query.pseudo;
 
    res.writeHead(200, {'Content-Type': 'text/html'});
    res.write(page);

e013f17f4c644d114ee78027d5180672187405d0
SIBAI <SIBAI@intechS1.fr>
2018-01-15 21:26:56 +0100

    lol

---
+++ Questionnaires/questionnaire6.js
@@ -0,0 +1,331 @@
+[
+	{
+		"question" : "en quelle année la france gagnais la coupe du monde ?",
+		"reponses": [
+						"1997",
+						"2006",
+						"2009",
+						"1998"
+					],
+		"br" : 3
+	}
+	,
+	{
+		"question" : "Quel sont les couleur du drapeau marocain ?",
+		"reponses": [
+						"bleu, blanc, rouge",
+						"rouge et vert",
+						"vert et blanc",
+						"rouge et blanc"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "a quel age est on sexuellement majeur ?",
+		"reponses": [
+						"15 ans",
+						"27 ans",
+						"18 ans",
+						"15 ans et 2 mois"
+					],
+		"br" : 3
+	}
+	,
+	{
+		"question" : "qui est le meilleur professeur de l'histoire ?",
+		"reponses": [
+						"P.Thire",
+						"Dumbledor",
+						"S.skinner",
+						"Kakashi seinsei"
+					],
+		"br" : 0
+	}
+	,
+	{
+		"question" : "De quel film vient la célèbre réplique imotep ?",
+		"reponses": [
+						"Asterix et Obelix, mission cléopatre",
+						"La haine",
+						"Scary movie",
+						"La momie"
+					],
+		"br" : 0
+	}
+	,
+	{
+		"question" : "en quelle année hitler a atteint le pouvoir ?",
+		"reponses": [
+						"1933",
+						"1927",
+						"2017",
+						"1937"
+					],
+		"br" : 0
+	}
+	,
+	{
+		"question" : "Qui est l'auteur de la célèbre chanson beat it ?",
+		"reponses": [
+						"Michael jackson",
+						"B.I.G",
+						"Booba",
+						"fatal bazooka"
+					],
+		"br" : 0
+	}
+	,
+	{
+		"question" : "Qui est l'auteur de 'i will always love you' ?",
+		"reponses": [
+						"Whitney Houston",
+						"Amy Winehouse",
+						"Alicia Keys",
+						"kenza farah"
+					],
+		"br" : 0
+	}
+	,
+	{
+		"question" : "Qui est l'entraineur du Real madrid ?",
+		"reponses": [
+						"Karim benzema",
+						"Zinedine Zidane",
+						"Henry Fumey-Humbert",
+						"Florentino Perez"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "en quelle année est sorti le seigneur des anneaux ?",
+		"reponses": [
+						"2001",
+						"2004",
+						"2002",
+						"1994"
+					],
+		"br" : 0
+	}
+	,
+	{
+		"question" : "En quelle année ont eu lieu les attentats du 11/09 ?",
+		"reponses": [
+						"2000",
+						"2001",
+						"2003",
+						"2002"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "Quel est le vrai nom du célèbre Mohamed Ali ?",
+		"reponses": [
+						"Ali",
+						"Cassus ",
+						"Momo ",
+						"Mohamed"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "En quelle année est mort michael jackson ?",
+		"reponses": [
+						"2009",
+						"2006",
+						"2018",
+						"2005"
+					],
+		"br" : 0
+	}
+	,
+	{
+		"question" : "De quel pays vient le célèbre chanteur Maitre gims ?",
+		"reponses": [
+						"Congo",
+						"Cameroun",
+						"France",
+						"Somalie"
+					],
+		"br" : 0
+	}
+	,
+	{
+		"question" : "De quel pays est originaire Omar Sy ?",
+		"reponses": [
+						"Senegal",
+						"France",
+						"Cote d'ivoire",
+						"Guinée Konacry"
+					],
+		"br" : 0
+	}
+	,
+	{
+		"question" : "Quel est le premier pokemon qu'a eu sacha ?",
+		"reponses": [
+						"Carabaffe",
+						"Apatou",
+						"Dracofeu",
+						"Pikachu"
+					],
+		"br" : 2
+	}
+	,
+	{
+		"question" : "Laquelle des trois n'est pas une religion monothéiste ?",
+		"reponses": [
+						"Islam",
+						"Boudhisme",
+						"Judaisme",
+						"Christianisme"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "Quel est le nom du druide du village d'Astérix?",
+		"reponses": [
+						"Assurancetourix",
+						"Panoramix",
+						"Asterix",
+						"Abraracourcix"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "Quel feuille se trouve au centre du drapeau canadien?",
+		"reponses": [
+						"erable",
+						"chêne",
+						"noisetier",
+						"cerisier"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "Laquelle des trois n'est pas une religion monothéiste ?",
+		"reponses": [
+						"Islam",
+						"Boudhisme",
+						"Judaisme",
+						"Christianisme"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "Lequel de ces 3 personnages n'a pas été président ?",
+		"reponses": [
+						"F.Fillon",
+						"J.Chirac",
+						"E.Macron",
+						"J-M Le Pen"
+					],
+		"br" : 3
+	}
+	,
+	{
+		"question" : "Qui a écrit les misérables?",
+		"reponses": [
+						"P.verlaine",
+						"V.Hugo",
+						"Robespierre",
+						"JUL"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "Qui a inventé les ordinateurs ?",
+		"reponses": [
+						"Einstein",
+						"Les Curie",
+						"A.Turing",
+						"Thalès"
+					],
+		"br" : 2
+	}
+	,
+	{
+		"question" : "Quel est le n°1 des ventes dans le  monde informatique ?",
+		"reponses": [
+						"Lenovo",
+						"Acer",
+						"Asus",
+						"Apple"
+					],
+		"br" : 3
+	}
+	,
+	{
+		"question" : "Qui est le n°1 des ventes en telephonie ?",
+		"reponses": [
+						"Samsung",
+						"Acer",
+						"Apple",
+						"Nokia"
+					],
+		"br" : 0
+	}
+	,
+	{
+		"question" : "Qui a écrit Les fourberies de Scapin ?",
+		"reponses": [
+						"Molière",
+						"Racine",
+						"Corneille",
+						"Bridou"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "Qui est le président des etats-unis ?",
+		"reponses": [
+						"Obama",
+						"Trump",
+						"Macron",
+						"Bush"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "Qui ont des chapeaux ronds ?",
+		"reponses": [
+						"Catalans",
+						"Auvergnats",
+						"Bretons",
+						"Gaulois"
+					],
+		"br" : 2
+	}
+	,
+	{
+		"question" : "Qui a construit la tour eiffel ?",
+		"reponses": [
+						"Leon Blum",
+						"Gustave eiffel",
+						"José eiffel",
+						"Gaston eiffel"
+					],
+		"br" : 1
+	}
+	,
+	{
+		"question" : "Qui est le courreur le plus rapide de l'histoire ?",
+		"reponses": [
+						"Usain Bolt",
+						"Jason Blake",
+						"Asapha Powell",
+						"Forest"
+					],
+		"br" : 0
+	}
+]
